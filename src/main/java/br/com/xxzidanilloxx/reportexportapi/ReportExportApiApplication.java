ea37a10542836c894d85a7da3f769548ed051864
xxzidanilloxx <wesleydanillo.silva@gmail.com>
2025-04-09 03:55:53 -0300

    chore: estrutura base do projeto

---
+++ src/main/java/br/com/xxzidanilloxx/reportexportapi/ReportExportApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.xxzidanilloxx.reportexportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportExportApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportExportApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/xxzidanilloxx/reportexportapi/ReportExportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xxzidanilloxx.reportexportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportExportApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
