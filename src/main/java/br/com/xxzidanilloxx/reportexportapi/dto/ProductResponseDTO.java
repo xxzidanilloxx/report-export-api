0d559f53c81f45c520c39b938ff9a5e1805a2a12
xxzidanilloxx <wesleydanillo.silva@gmail.com>
2025-04-10 02:35:25 -0300

    chore: adiciona @JsonFormat para formatação de data e hora nos campos createdAt e updatedAt

--- src/main/java/br/com/xxzidanilloxx/reportexportapi/dto/ProductResponseDTO.java
+++ src/main/java/br/com/xxzidanilloxx/reportexportapi/dto/ProductResponseDTO.java
@@ -1,6 +1,7 @@
 package br.com.xxzidanilloxx.reportexportapi.dto;
 
 import br.com.xxzidanilloxx.reportexportapi.enumeration.Category;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -14,6 +15,8 @@ public record ProductResponseDTO(
         Category category,
         BigDecimal price,
         Boolean isActive,
+        @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
         LocalDateTime createdAt,
+        @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
         LocalDateTime updatedAt) {
 }
